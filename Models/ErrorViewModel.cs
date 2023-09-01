c9623807f7f5e3693fa54dfcdd3548c8a367f69d
Scarstack Solution <137973466+scarstacksolution@users.noreply.github.com>
2023-08-31 22:28:56 -0500

    Version 3 first commit
    New Local repository feature

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Aug31_Ver1_WebAp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
