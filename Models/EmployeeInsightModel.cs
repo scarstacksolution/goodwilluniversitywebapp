05c7118925e6b8a9bb4c9af0eae3d2d1442754c0
olu <137973466+scarstacksolution@users.noreply.github.com>
2023-09-01 01:23:02 -0500

    Commit for For Updated Index View and Model classes

---
+++ Models/EmployeeInsightModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Aug31_Ver1_WebAp.Models
+{
+	public class EmployeeInsightModel
+	{
+		public EmployeeInsightModel()
+		{
+		}
+	}
+}
+
---
+++ Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Aug31_Ver1_WebAp.Models
+{
+    public class Person
+    {
+        public Person()
+        { }
+
+        public int id { get; set; }
+
+        public string? firstname { get; set; }
+
+        public string? lastname { get; set; }
+
+        public int? age { get; set; }
+
+        public string? gender { get; set; }
+    }
+}
+
