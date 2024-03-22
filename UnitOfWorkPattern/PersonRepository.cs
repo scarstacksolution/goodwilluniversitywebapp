c19dc229352e05a904e7d30c6db326340449b4a0
olu <adeoyeseunengineer@hotmail.com>
2024-03-21 19:30:05 -0500

    1st code development changes

--- UnitOfWorkPattern/PersonRepository.cs
+++ UnitOfWorkPattern/PersonRepository.cs
@@ -24,24 +24,6 @@ namespace Aug31_Ver1_WebAp.UnitOfWorkPattern
         /// <returns></returns>
         public Person GetPersonById(int id)
         {
-            var _employees = new List<Person>();
-
-            IEnumerable<Person> employees = (IQueryable<Person>)_context.Person
-<<<<<<< HEAD
-                .OrderBy(e => e.id)
-                .AsNoTracking(); 
-=======
-                .OrderBy(e => e.id);
-            //.AsNoTracking(); 
->>>>>>> 4e2e06dc684b47cbdc86b17e0ee70a6f412b5f03
-
-            foreach (var employee in employees)
-            {
-                _employees.Add(employee);
-            }
-
-            var data = _employees;
-
             Person person = _context.Person.First(e => e.id == id);
             return person;
         }
